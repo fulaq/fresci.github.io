0e9d32be21d851ce15555c487252f76e4a68f925
fulaq <112454024+fulaq@users.noreply.github.com>
2026-01-01 19:10:21 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'fresci-core-v3';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/data.js',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Noto+Serif:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).then(response => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    if(e.request.method === 'GET') cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
